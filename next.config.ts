84e40f9375dbce37b46e73bdeac867ce7a95d275
trtum01 <TRtum123>
2026-01-15 15:23:15 +0700

    Add output configuration to Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   reactStrictMode: true,
   assetPrefix: isProd ? '/nonrit08.github.io/' : '',
 };
